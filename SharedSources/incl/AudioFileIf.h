a5d1e68b583e8038fd35d3a4087de4a0a24d4037
lerch <lerch@zplane.de>
2013-12-07 20:27:39 -0500

    - minor fixes in CMakeLists.txt

--- SharedSources/incl/AudioFileIf.h
+++ SharedSources/incl/AudioFileIf.h
@@ -18,15 +18,17 @@ public:
     enum FileFormat_t
     {
         kFileFormatWav,
+        kFileFormatAiff,
         kFileFormatRaw,
+        kFileFormatUnknown,
 
         kNumFileFormats
     };
     enum BitStream_t
     {
         kFileBitStreamInt16,
-        //kFileBitStreamInt32,
-        //kFileBitStreamFloat32,
+        kFileBitStreamFloat32,
+        kFileBitStreamUnknown,
 
         kNumWordLengths
     };
@@ -38,60 +40,124 @@ public:
         float           fSampleRate;
     };
 
-    CAudioFileIf ();;
-    virtual ~CAudioFileIf ();
+    static Error_t createInstance (CAudioFileIf*& pCInstance);
+    static Error_t destroyInstance (CAudioFileIf*& pCInstance);
 
-    Error_t openFile (std::string cAudioFileName, FileIoType_t eIoType, FileSpec_t const *psFileSpec = 0);
-    Error_t closeFile ();
+    virtual Error_t openFile (std::string cAudioFileName, FileIoType_t eIoType, FileSpec_t const *psFileSpec = 0) = 0;
+    virtual Error_t closeFile () = 0;
+
+    virtual Error_t readData (float **ppfAudioData, int &iNumFrames);
+    virtual Error_t writeData (float **ppfAudioData, int iNumFrames);
 
     Error_t getFileSpec (FileSpec_t &sFileSpec);
 
-    Error_t setPosition (long long iFrame = 0);
+    virtual Error_t setPosition (long long iFrame = 0);
     Error_t setPosition (double dTimeInS = .0);
 
     Error_t setClippingEnabled (bool bIsEnabled = true);
+    bool isClippingEnabled () {return m_bWithClipping;};
 
     Error_t getPosition (long long &iFrame);
     Error_t getPosition (double &dTimeInS);
     Error_t getLength (long long &iLengthInFrames) ;
     Error_t getLength (double &dLengthInSeconds) ;
 
-    Error_t readData (float **ppfAudioData, int &iNumFrames);
-    Error_t writeData (float **ppfAudioData, int iNumFrames);
+    virtual bool isEof () = 0;
+    virtual bool isOpen () = 0;
+    virtual bool isInitialized ();
+    virtual Error_t resetInstance (bool bFreeMemory = false);
 
+protected:
+
+    CAudioFileIf ();
+    virtual ~CAudioFileIf ();
+    virtual Error_t freeMemory ();
+    virtual Error_t allocMemory ();
+
+    Error_t setInitialized (bool bInitialized = true);
+    Error_t setIoType (FileIoType_t eIoType);
+    FileIoType_t getIoType () const;
+    Error_t setFileSpec (const FileSpec_t *pFileSpec);
+    int getNumChannels () const;
+    long long convFrames2Bytes (long long iNumFrames);
+    long long convBytes2Frames (long long iNumFrames);
+    Error_t setNumBytesPerSample (int iNumBytes);
+    int getNumBytesPerSample () const;
+    int getNumBitsPerSample () const {return (m_iNumBytesPerSample<<3);}
+
+    static const int    m_kiDefBlockLength;     //!< buffer length for read and write operations
+
+private:
+    virtual Error_t initDefaults ();
+
+    virtual int readDataIntern (float **ppfAudioData, int iNumFrames) = 0;
+    virtual int writeDataIntern (float **ppfAudioData, int iNumFrames) = 0;
+    virtual long long getLengthIntern() = 0;
+    virtual long long getPositionIntern() = 0;
+    virtual Error_t setPositionIntern( long long iFrame ) = 0;
+
+    FileSpec_t      m_sCurrFileSpec;            //!< current file specifications
+    FileIoType_t    m_eIoType;                  //!< read or write
+
+    bool            m_bWithClipping;            //!< true if abs(values ) > 1 should be clipped
+    bool            m_bIsInitialized;           //!< true if initialized
+    int             m_iNumBytesPerSample;     //!< number of bytes per sample for the raw pcm IO option without sndlib
+
+};
+
+class CAudioFileRaw : public CAudioFileIf
+{
+public:
+    CAudioFileRaw ();
+    virtual ~CAudioFileRaw ();
+
+    Error_t openFile (std::string cAudioFileName, FileIoType_t eIoType, FileSpec_t const *psFileSpec = 0);
+    Error_t closeFile ();
     bool isEof ();
-
-    Error_t resetInstance (bool bFreeMemory = false);
+    bool isOpen ();
 
 private:
     Error_t freeMemory ();
     Error_t allocMemory ();
-    Error_t initDefaults ();
+    int readDataIntern (float **ppfAudioData, int iNumFrames);
+    int writeDataIntern (float **ppfAudioData, int iNumFrames);
+    long long getLengthIntern();
+    long long getPositionIntern();
+    Error_t setPositionIntern( long long iFrame );
 
-    int readDataRaw (float **ppfAudioData, int &iNumFrames);
-    int writeDataRaw (float **ppfAudioData, int iNumFrames);
-    long long getLengthRaw() ;
-    long long getPositionRaw();
-    Error_t setPositionRaw( long long iFrame );
+    float scaleUp (float fSample2Clip);
+    float scaleDown (float fSample2Scale);
 
-
-    FileSpec_t      m_sCurrFileSpec;            //!< current file specifications
-
-    bool            m_bWithClipping;            //!< true if abs(values ) > 1 should be clipped
-    bool            m_bIsInitialized;           //!< true if initialized
-
-    static const int    m_kiDefBlockLength;     //!< buffer length for read and write operations
-
-/**
- * \defgroup WOsndlib member variables for raw audio IO without sndlib
- * @{
- */
     std::fstream        m_File;                 //!< raw pcm file handle
-    static const int    m_iNumOfBytesPerSample; //!< number of bytes per sample for the raw pcm IO option without sndlib
     short               *m_piTmpBuff;           //!< temporary buffer for 16 bit integer values
-
-/**@}*/
-
 };
 
+
+class CAudioFileSndLib : public CAudioFileIf
+{
+public:
+    CAudioFileSndLib ();
+    virtual ~CAudioFileSndLib ();
+
+    Error_t openFile (std::string cAudioFileName, FileIoType_t eIoType, FileSpec_t const *psFileSpec = 0);
+    Error_t closeFile ();
+    bool isEof ();
+    bool isOpen ();
+    Error_t freeMemory ();
+    Error_t allocMemory ();
+
+private:
+    int readDataIntern (float **ppfAudioData, int iNumFrames);
+    int writeDataIntern (float **ppfAudioData, int iNumFrames);
+    long long getLengthIntern();
+    long long getPositionIntern();
+    Error_t setPositionIntern( long long iFrame );
+
+    int m_FileHandle;                           //!< sndlib file handle
+
+    long long m_lFrameCnt;
+    long long m_lFileLength;
+    double    **m_ppdTmpBuff;                     //!< temporary buffer for double values
+
+};
 #endif // #if !defined(__AudioFileIf_hdr__)
