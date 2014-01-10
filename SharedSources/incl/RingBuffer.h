da96f7bd6ed4a2f032656533d0c0a5181aa6c983
lerch <lerch@zplane.de>
2014-01-10 15:51:55 -0500

    - cosmetics

--- SharedSources/incl/RingBuffer.h
+++ SharedSources/incl/RingBuffer.h
@@ -9,11 +9,12 @@ class CRingBuffer
 public:
     CRingBuffer (int iBufferLengthInSamples) :
         m_iBuffLength(iBufferLengthInSamples),
-        m_iReadIdx(0),                          // note: when the read index equals the write index you actually don't know whether the buffer is full or empty -- you are on your own!
+        m_iReadIdx(0),                          
         m_iWriteIdx(0),
         m_ptBuff(0)
     {
         assert(iBufferLengthInSamples > 0);
+
         m_ptBuff        = new T [m_iBuffLength];
         resetInstance();
     };
@@ -24,12 +25,19 @@ public:
         m_ptBuff    = 0;
     };
 
+    /*! add a new value of type T and increment write index
+    \param T tNewValue the new value
+    \return void
+    */
     void putValuePostInc (T tNewValue)
     {
         m_ptBuff[m_iWriteIdx]   = tNewValue;
         incIdx(m_iWriteIdx);
     };
 
+    /*! return the value of the current read index and increment the read pointer
+    \return float the value from the read index
+    */
     float getValuePostInc ()
     {
         int iCurrIdx    = m_iReadIdx;
@@ -37,6 +45,9 @@ public:
         return m_ptBuff[iCurrIdx];
     };
 
+    /*! set buffer content and indices to 0
+    \return void
+    */
     void resetInstance ()
     {
         CUtil::setZero(m_ptBuff, m_iBuffLength);
@@ -44,6 +55,9 @@ public:
         m_iWriteIdx = 0;
     }
 
+    /*! returns the number of values currently buffered (note: 0 could also mean the buffer is full!)
+    \return int
+    */
     int getNumValuesInBuffer ()
     {
         return (m_iWriteIdx - m_iReadIdx + m_iBuffLength)%m_iBuffLength;
@@ -54,10 +68,10 @@ private:
         iIdx    = (iIdx + 1) % m_iBuffLength;
     };
 
-    int     m_iBuffLength,
-        m_iReadIdx,
-        m_iWriteIdx;
+    int     m_iBuffLength,          //!< length of the internal buffer
+        m_iReadIdx,                 //!< current read index
+        m_iWriteIdx;                //!< current write index
 
-    T       *m_ptBuff;
+    T       *m_ptBuff;              //!< data buffer
 };
 #endif // __RingBuffer_hdr__
