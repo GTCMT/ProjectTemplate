63a8dff1a6b0680e6d3a2d1239f24ed23663b687
lerch <lerch@zplane.de>
2014-01-16 17:37:46 -0500

    - added put, get, getWrite/ReadIdx, setWrite/ReadIdx

--- SharedSources/incl/RingBuffer.h
+++ SharedSources/incl/RingBuffer.h
@@ -25,7 +25,7 @@ public:
         m_ptBuff    = 0;
     };
 
-    /*! add a new value of type T and increment write index
+    /*! add a new value of type T to write index and increment write index
     \param T tNewValue the new value
     \return void
     */
@@ -35,7 +35,16 @@ public:
         incIdx(m_iWriteIdx);
     };
 
-    /*! return the value of the current read index and increment the read pointer
+    /*! add a new value of type T to write index
+    \param T tNewValue the new value
+    \return void
+    */
+    void put(T tNewValue)
+    {
+        m_ptBuff[m_iWriteIdx]   = tNewValue;
+    }
+
+    /*! return the value at the current read index and increment the read pointer
     \return float the value from the read index
     */
     T getPostInc ()
@@ -44,6 +53,14 @@ public:
         incIdx(m_iReadIdx);
         return m_ptBuff[iCurrIdx];
     };
+
+    /*! return the value at the current read index
+    \return float the value from the read index
+    */
+    T get () const
+    {
+        return m_ptBuff[m_iReadIdx];
+    }
 
     /*! set buffer content and indices to 0
     \return void
@@ -55,6 +72,40 @@ public:
         m_iWriteIdx = 0;
     }
 
+    /*! return the current index for writing/put
+    \return int
+    */
+    int getWriteIdx () const
+    {
+        return m_iWriteIdx;
+    }
+
+    /*! move the write index to a new position
+    \param int iNewWriteIdx: new position
+    \return void
+    */
+    void setWriteIdx (int iNewWriteIdx)
+    {
+        incIdx(m_iWriteIdx, iNewWriteIdx - m_iWriteIdx);
+    }
+
+    /*! return the current index for reading/get
+    \return int
+    */
+    int getReadIdx () const
+    {
+        return m_iReadIdx;
+    }
+
+    /*! move the read index to a new position
+    \param int iNewReadIdx: new position
+    \return void
+    */
+    void setReadIdx (int iNewReadIdx)
+    {
+        incIdx(m_iReadIdx, iNewReadIdx - m_iReadIdx);
+    }
+
     /*! returns the number of values currently buffered (note: 0 could also mean the buffer is full!)
     \return int
     */
@@ -62,6 +113,7 @@ public:
     {
         return (m_iWriteIdx - m_iReadIdx + m_iBuffLength)%m_iBuffLength;
     }
+
 private:
     void incIdx (int &iIdx, int iOffset = 1)
     {
