54be6a742dcfac1e38f76e3040b5654e8eff0933
lerch <lerch@zplane.de>
2014-01-26 15:22:29 -0500

    - added VST plugin target for Windows (when WITH_JUCE is enabled)

--- cmake.modules/FindJuce.cmake
+++ cmake.modules/FindJuce.cmake
@@ -2,7 +2,6 @@
 IF( NOT JUCE_FOUND )
 
 	FIND_PATH(JUCE_ROOT_DIR juce.h PATHS ${CMAKE_SOURCE_DIR}/3rdPartyLibs/JUCE )
-#	FIND_PATH(JUCE_APP_CONFIG_DIR AppConfig.h PATHS ${CMAKE_SOURCE_DIR}/JuceLibraryCode)
 
 	set(JUCE_MODULES juce_core juce_data_structures juce_events juce_graphics juce_gui_basics juce_gui_extra juce_audio_basics juce_audio_devices juce_audio_formats juce_audio_plugin_client juce_audio_processors juce_audio_utils)
 	
@@ -15,6 +14,8 @@ IF( NOT JUCE_FOUND )
 		file(GLOB JUCE_HEADERS_${dir} RELATIVE ${CMAKE_SOURCE_DIR}/MyProject/src/MyProject ${JUCE_ROOT_DIR}/modules/${dir}/*.h )
 		
 	endforeach(dir ${JUCE_MODULES})
+
+	FIND_PATH(JUCE_PLUGIN_WRAPPER_DIR VST PATHS ${JUCE_ROOT_DIR}/modules/juce_audio_plugin_client/)
 
 
 	include (FindPackageHandleStandardArgs)
