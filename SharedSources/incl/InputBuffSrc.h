107d4fb8de78395a6df7a43203d1f909197947dd
lerch <lerch@zplane.de>
2014-02-26 13:43:31 -0500

    - implemented dummy declaration and declared member variables - changed class to template

--- SharedSources/incl/InputBuffSrc.h
+++ SharedSources/incl/InputBuffSrc.h
@@ -4,13 +4,13 @@
 
 #include "ErrorDef.h"
 
-/*! added buffer class for faster implementation of variable input block size vs. constant processing block size
+/*! buffer class for faster implementation of variable input block size vs. constant processing block size
 
     USAGE
 
         // add new input data of any length
         pInputBuffSrcIf->setDataPtr2Hold(apfTmpPtr, iInpLength);
-        while (pInputBuffSrcIf->getBlock(ppfProcBuff, kiProcBuffLength))
+        while (pInputBuffSrcIf->getBlock(ppfProcBuff, kiProcBuffLength, kiProcBuffLength))
         {
             // do processing on ppfProcBuff with FIXED length kiProcBuffLength          
         }
@@ -21,22 +21,52 @@
         pInputBuffSrcIf->flush(ppfProcBuff);
 */
 
-class CInputBuffSrcIf
+template <class T> 
+class CInputBuffSrc
 {
 public:
-    CInputBuffSrcIf (int iNumOfChannels, int iMaxOutputLength = 1024, int iInitialLatency = 0){};
-    virtual ~CInputBuffSrcIf (){};
+    CInputBuffSrc (int iNumOfChannels, int iMaxOutputLength = 1024, int iInitialLatency = 0) : m_iNumChannels(iNumOfChannels),
+        m_iMaxOutLength(iMaxOutputLength),
+        m_iNumFramesBuffered(iInitialLatency),
+        m_ppfBufferedData(0),
+        m_ppfDataPtr(0)
+    {
+    };
+    virtual ~CInputBuffSrc ()
+    {};
 
-    virtual Error_t setDataPtr2Hold (float **ppNewData, int iNumOfElements) = 0;
-    virtual Error_t releaseDataPtr () = 0;
+    Error_t setDataPtr2Hold (T **ppNewData, int iNumOfElements)
+    {
+        return kNoError;
+    };
     
-    virtual Error_t getBlock (float **ppBlock, int iOutputBufferSize, int iIncIdxBy = 0) = 0;
+    Error_t releaseDataPtr ()
+    {
+        return kNoError;
+    };
+    
+    Error_t getBlock (T **ppBlock, int iOutputBufferSize, int iIncIdxBy = 0)
+    {
+        return kNoError;
+    };
 
-    virtual Error_t reset () = 0;
+    Error_t reset ()
+    {
+        return kNoError;
+    };
 
-    virtual int  flush (float **ppBlock) = 0;
+    int  flush (T **ppBlock)
+    {
+        return 0;
+    };
+
+private:
+    int     m_iNumChannels;             //!< number of channels in data
+    int     m_iMaxOutLength;            //!< maximum number of output frames
+    int     m_iNumFramesBuffered;       //!< number of frames currently in internal buffer
+    T       **m_ppfBufferedData;        //!< internal buffer
+    T       **m_ppfDataPtr;             //!< pointer to external data to be managed
 };
-
 
 
 #endif //#define __InputBuffSrcIf_HEADER_INCLUDED__
