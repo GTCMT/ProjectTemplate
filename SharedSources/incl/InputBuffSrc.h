9942da738668d56a1a4082ddea4b7cb19f33a251
lerch <lerch@zplane.de>
2014-03-05 09:49:40 -0500

    - added a few comments for clarity

--- SharedSources/incl/InputBuffSrc.h
+++ SharedSources/incl/InputBuffSrc.h
@@ -35,26 +35,47 @@ public:
     virtual ~CInputBuffSrc ()
     {};
 
+    /*! set new external data pointer
+    \param T * * ppNewData
+    \param int iNumOfElements
+    \return Error_t
+    */
     Error_t setDataPtr2Hold (T **ppNewData, int iNumOfElements)
     {
         return kNoError;
     };
     
+    /*! store remaining frames and release external data pointer
+    \return Error_t
+    */
     Error_t releaseDataPtr ()
     {
         return kNoError;
     };
     
+    /*! return current block of frames if available
+    \param T * * ppBlock
+    \param int iOutputBufferSize
+    \param int iIncIdxBy
+    \return bool
+    */
     bool getBlock (T **ppBlock, int iOutputBufferSize, int iIncIdxBy = 0)
     {
         return false;
     };
 
+    /*! clear all internal buffer and reset member variables
+    \return Error_t
+    */
     Error_t reset ()
     {
         return kNoError;
     };
 
+    /*! return the remaining frames from internal buffers
+    \param T * * ppBlock
+    \return int
+    */
     int  flush (T **ppBlock)
     {
         return 0;
@@ -62,12 +83,13 @@ public:
 
 private:
     int     m_iNumChannels;                 //!< number of channels in data
+    T       **m_pptInternalBuffer;          //!< internal buffer
     int     m_iMaxInternalBufferLength;     //!< maximum number of output frames
     int     m_iNumFramesInternalBuffer;     //!< number of frames currently in internal buffer
-    T       **m_pptInternalBuffer;          //!< internal buffer
+
     T       **m_pphtExternalData;           //!< pointer to external data to be managed
     int     m_iExternalDataReadIdx;         //!< current read index in m_pphtExternalData
-    int     m_iNumFramesExternalData;       //!< 
+    int     m_iNumFramesExternalData;       //!< number of frames in m_pphtExternalData
 };
 
 
